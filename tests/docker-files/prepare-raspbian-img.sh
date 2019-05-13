42bb84c329c961412eca81e088879c0fe1a86c29
Lluis Campos <lluis.campos@northern.tech>
2019-05-13 14:29:55 +0200

    Parametrize the Raspbian version to use in the build and test
    And minor optimizations in Dockerfile
    
    Changelog: None
    
    Signed-off-by: Lluis Campos <lluis.campos@northern.tech>

--- tests/docker-files/prepare-raspbian-img.sh
+++ tests/docker-files/prepare-raspbian-img.sh
@@ -5,14 +5,45 @@
 
 set -e
 
+show_help_and_exit() {
+  cat << EOF
+Usage: $0 raspbian-version
+
+Arguments:
+    raspbian-version    - Official Raspbian Stretch version, for example 2019-04-08
+EOF
+  exit 1
+}
+
+version=""
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -*)
+      echo "Error: unsupported option $1"
+      show_help_and_exit_error
+      ;;
+    *)
+      version="$1"
+      shift
+      ;;
+  esac
+done
+
+if [ -z "$version" ]; then
+	show_help_and_exit
+fi
+
 currdir=$(pwd)
 scriptdir=$(cd `dirname $0` && pwd)
 workdir=$(mktemp -d)
 
-#TODO: Verify image with md5sum
-if [ -f 2019-04-08-raspbian-mender-testing.img ]; then
+raspbian_filename_zip="${version}-raspbian-stretch-lite.zip"
+raspbian_filename_img="${version}-raspbian-stretch-lite.img"
+raspbian_mender_filename_img="${version}-raspbian-mender-testing.img"
+
+if [ -f ${currdir}/${raspbian_mender_filename_img} ]; then
     echo "Found testing image in current directory. Exiting"
-    exit 0;
+    exit 0
 fi
 
 # Get superuser privilages to be able to mount the SD image
@@ -20,26 +51,32 @@ sudo true
 
 cd ${workdir}
 
+# For some reason, Raspbian version 2019-04-08 is in a folder named 2019-04-09
+if [ ${version} = "2019-04-08" ]; then
+    raspbian_url="https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2019-04-09/${raspbian_filename_zip}"
+else
+    raspbian_url="https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-${version}/${raspbian_filename_zip}"
+fi
+
 echo "##### Donwloading and extracting..."
-wget https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2019-04-09/2019-04-08-raspbian-stretch-lite.zip
-unzip 2019-04-08-raspbian-stretch-lite.zip
+wget ${raspbian_url}
+unzip ${raspbian_filename_zip}
 wget https://raw.githubusercontent.com/dhruvvyas90/qemu-rpi-kernel/master/kernel-qemu-4.14.79-stretch
 wget https://raw.githubusercontent.com/dhruvvyas90/qemu-rpi-kernel/master/versatile-pb.dtb
 
 echo "##### Preparing image for tests..."
-boot_start=$(fdisk -l 2019-04-08-raspbian-stretch-lite.img | grep Linux | tr -s ' ' | cut -d ' ' -f2)
-sector_size=$(fdisk -l 2019-04-08-raspbian-stretch-lite.img | grep '^Sector' | cut -d' ' -f4)
+boot_start=$(fdisk -l ${raspbian_filename_img} | grep Linux | tr -s ' ' | cut -d ' ' -f2)
+sector_size=$(fdisk -l ${raspbian_filename_img} | grep '^Sector' | cut -d' ' -f4)
 offset=$(expr $boot_start \* $sector_size)
 mkdir img-rootfs
-sudo mount -o loop,offset=$offset 2019-04-08-raspbian-stretch-lite.img img-rootfs
+sudo mount -o loop,offset=$offset ${raspbian_filename_img} img-rootfs
 
 sudo mkdir img-rootfs/home/pi/.ssh
 cat ${scriptdir}/ssh-keys/key.pub | sudo tee img-rootfs/home/pi/.ssh/authorized_keys
 sudo ln -s /lib/systemd/system/ssh.service img-rootfs/etc/systemd/system/multi-user.target.wants/ssh.service
-sync
 sudo umount img-rootfs
 
-mv 2019-04-08-raspbian-stretch-lite.img ${currdir}/2019-04-08-raspbian-mender-testing.img
+mv ${raspbian_filename_img} ${currdir}/${raspbian_mender_filename_img}
 mv kernel-qemu-4.14.79-stretch ${currdir}/
 mv versatile-pb.dtb ${currdir}/
 
