9a3e54111134f0893878d0f777b6c3a2c9ecb42c
Lluis Campos <lluis.campos@northern.tech>
2023-08-07 11:58:37 +0200

    chore: Deprecate Ubuntu Bionic debian packages
    Ubuntu Bionic standard LTS support is EOL.
    
    Ticket: QA-600
    
    Signed-off-by: Lluis Campos <lluis.campos@northern.tech>

--- scripts/install-mender.sh
+++ scripts/install-mender.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2022 Northern.tech AS
+# Copyright 2023 Northern.tech AS
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
@@ -507,9 +507,6 @@ check_dist_and_version() {
                 ;;
                 focal)
                     DIST_VERSION="focal"
-                ;;
-                bionic)
-                    DIST_VERSION="bionic"
                 ;;
                 *)
                     echo "ERROR: your distribution's version ($DIST_VERSION) is either not recognized or not supported."
