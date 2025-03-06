8246c8c9909fb362ab8ec9f784368719b6f884e3
Lluis Campos <lluis.campos@northern.tech>
2025-03-06 13:40:01 +0100

    refac: Rework CI/CD to selectively build/test/publish the packages
    The default behaviour does not change: a pipeline with the default
    values (all versions to `master`) will build, test and potentially
    publish all packages.
    
    However on demand, by setting any of the packages versions to empty
    string, these won't be built/tested/published. This allows for release
    builds were only one or a subset of packages are needed.
    
    Two main changes:
    * On the test level, mark all tests with per-package markers and add
      some logic in the collection of tests to skip the once we don't have
      version information about
    * On the CI/CD level, refactor the whole pipeline to produce CI jobs per
      package. This seems like an overkill but the explicitness of it is
      preferable to complex alternatives such a dynamic pipelines.
    
    Ticket: QA-689
    
    Signed-off-by: Lluis Campos <lluis.campos@northern.tech>

--- tests/conftest.py
+++ tests/conftest.py
@@ -44,7 +44,9 @@ def setup_test_container_props(request):
 
 def pytest_addoption(parser):
     for pkg in TEST_PACKAGES:
-        parser.addoption(f"--{pkg}-version", required=False)
+        parser.addoption(
+            f"--{pkg}-version", required=False, help=f"Leave empty to skip {pkg} tests"
+        )
         parser.addoption(f"--{pkg}-deb-version", required=False)
 
     parser.addoption(
@@ -117,6 +119,12 @@ def pytest_collection_modifyitems(config, items):
         for item in items:
             if "commercial" in item.keywords:
                 item.add_marker(skip_commercial)
+
+    for pkg in TEST_PACKAGES:
+        if not config.getoption(f"--{pkg}-version"):
+            for item in items:
+                if pkg.replace("-", "_") in item.keywords:
+                    item.add_marker(pytest.mark.skip(reason=f"Skip {pkg} tests"))
 
 
 @pytest.fixture(scope="session")
