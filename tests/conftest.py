3e5760a3a09071317c337448f70d544b02307a53
Vratislav Podzimek <vratislav.podzimek+auto-signed@northern.tech>
2025-08-01 18:20:46 +0200

    tests: Use client 3.5.3 for addon packages tests
    There is no client 3.2.1 for armhf so we need to use a newer
    one. However, to test backwards compatibility, we use the latest
    client before the version 4.0.
    
    Ticket: QA-1138
    Changelog: none
    Signed-off-by: Vratislav Podzimek <vratislav.podzimek+auto-signed@northern.tech>

--- tests/conftest.py
+++ tests/conftest.py
@@ -158,11 +158,12 @@ def mender_dist_packages_versions(request):
     }
 
 
-# Required for mender_test_containers/conftest.py::setup_mender_configured, which
-# is only used on addons packages tests. Use version 3.2.1 (mender-connect dependency)
+# Required for mender_test_containers/conftest.py::setup_mender_configured,
+# which is only used on addons packages tests. Use version 3.5.3 as the latest
+# version < 4.0.
 @pytest.fixture(scope="session")
 def mender_deb_version(request):
-    return "3.2.1"
+    return "3.5.3"
 
 
 def min_version_impl(request, marker, min_version):
